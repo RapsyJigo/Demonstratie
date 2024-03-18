a6d3c5b0c38be65df55f86b24898338943d95e5c
Darius Takacs <darius_takacs@yahoo.com>
2024-03-18 22:40:25 +0200

    first

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "test.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << test();
+    return 0;
+}
---
+++ test.h
@@ -0,0 +1,9 @@
+#ifndef TEST_H_INCLUDED
+#define TEST_H_INCLUDED
+int test()
+{
+    return 0;
+}
+
+
+#endif // TEST_H_INCLUDED
